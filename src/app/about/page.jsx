fc056af0e4f90042639fd96b9964e797adee2fa6
Anne-Sophie <annesophie.faustino@gmail.com>
2024-04-09 18:09:12 +0400

    create about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto p-3 space-y-4">
+      <h1 className="text-2xl font-medium text-amber-500">About</h1>
+
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum
+        exercitationem vel aliquam eius laborum voluptatum animi. Eos laboriosam
+        dignissimos, commodi pariatur animi odit! Illo fugit explicabo eligendi
+        nulla ullam velit.
+      </p>
+
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum
+        exercitationem vel aliquam eius laborum voluptatum animi. Eos laboriosam
+        dignissimos, commodi pariatur animi odit! Illo fugit explicabo eligendi
+        nulla ullam velit. Lorem ipsum dolor sit amet consectetur, adipisicing
+        elit. Sunt molestias adipisci, perspiciatis veniam fugit ab recusandae
+        eaque ut aliquid, voluptas minus! Amet nostrum quos est quisquam enim,
+        animi quibusdam aperiam. Commodi mollitia voluptatem vitae similique
+        ipsum nulla cum molestias facere possimus! Ab necessitatibus rem
+        adipisci soluta atque impedit obcaecati recusandae minus. Consectetur
+        minima eius error consequuntur possimus labore nostrum dolorem.
+      </p>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis,
+        itaque aliquid? Iste nesciunt quasi quis ad illo vitae, quibusdam
+        recusandae. Impedit rerum deleniti accusantium ut vel cumque ad possimus
+        aliquid. Deserunt recusandae numquam rerum, aperiam nam dolores dolorum
+        voluptatem molestias. Suscipit odio dignissimos soluta ab, sapiente
+        quisquam culpa sunt provident pariatur, minus atque perferendis cum
+        laboriosam velit laborum quibusdam sequi? Quaerat natus quasi
+        consectetur quis, laboriosam beatae fugit eaque possimus in cum quae
+        nisi soluta nihil at, maxime repellendus iusto. Beatae consequuntur
+        incidunt officiis quas velit odio laborum accusantium tenetur! Non
+        corrupti fugit nam alias asperiores velit odio ex tempora architecto
+        deleniti error exercitationem ipsam beatae, facilis expedita ipsa eius
+        quam possimus impedit laboriosam qui maiores quasi. Dolorum, voluptatem
+        dicta? Ab blanditiis odio pariatur repudiandae et totam, cum atque ullam
+        fuga, aperiam perspiciatis at tempora dolore dolores soluta voluptatibus
+        amet maiores error, dolorem quas eos nulla odit. Beatae, est iusto?
+      </p>
+    </div>
+  );
+}
